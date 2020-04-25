ef5bba40cd45066fca77f32466d8023c41e0c13a
Dalton-cmd <Dalton.t.hoekstra@gmail.com>
2020-04-25 15:14:36 -0500

    Created an inefficient RegEx function. Started a RegEx function using the "Re" package

---
+++ Miscellaneous Testing Scripts - Playground/Inefficient RegEx.py
@@ -0,0 +1,26 @@
+#basic RegEx function to find a phone number
+def isPhoneNumber(text):
+    if len(text) != 12:                                                         #Checks that the input is at least 12 characters long
+        return False
+    for i in range(0, 3):                                                       #$Checks that there is an area code
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':                                                          #Checks that there are dashes
+        return False
+    for i in range(4,7):
+        if not text[i].isdecimal():
+            return False                                                        #No first 3 digits
+    for i in range(8,12):
+        if not text[i].isdecimal():
+            return False                                                        #Missing the last 4 digits
+    return True
+
+message = 'Call me at 555-555-4555 tomorrow, or at 444-444-4544. thanks'
+foundNumber = False
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('phone number found ' + chunk)
+        foundNumber = True
+if not foundNumber:
+    print('Could not find any phone numbers')
